30d0ee58c0b8fb26f1b123adcfd5cf12b2135846
Roshan Guragain <twistfreaky@gmail.com>
2023-05-02 10:06:15 +0545

    ProductController-  create product

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,44 @@
+import Product from '../models/product.schema'
+
+import fs from 'fs'
+import tryCatchHandler from '../services/tryCatchHandler'
+import WhereClause from '../utils/whereClause';
+const cloudinary = require("cloudinary").v2
+
+// create product --admin
+export const createProduct = tryCatchHandler(async(req, res, next)=>{
+
+    // images
+    let imageArray =[];
+
+    if(!req.files){
+        return next(new CustomError('images are required', 400))
+    }
+
+    if (req.files){
+        for(let i=0; i< req.files.photos.length; i++){
+        // const element = req.files.photos[i];
+        // cloudinary
+            let result = await cloudinary.uploader.upload(req.files.photos[i].
+            tempFilePath, {
+                folder: 'products'
+            }
+            );
+
+            imageArray.push({
+                id: result.public_id,
+                secure_url:result.secure_url
+            })
+        }
+    }
+
+    req.body.photos = imageArray;
+    req.body.user = req.user.id
+
+    const product =await Product.create(req.body)
+    res.status(200).json({
+        success:true,
+        product,
+    })
+});
+
