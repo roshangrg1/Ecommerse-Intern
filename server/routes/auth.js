0cad95336adc8b4dbcc6de00fd7627c0c9ee938a
Roshan Guragain <twistfreaky@gmail.com>
2023-05-11 08:53:58 +0545

    Router added : logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
 import express from 'express'
 const router =express.Router();
 
-import {signUp,}from '../controllers/auth.controller.js'
+import {signUp,login, logout}from '../controllers/auth.controller.js'
 
-
+import { isLoggedIn } from '../middlewares/auth.middleware.js';
 
 router.post('/signup',signUp)
+router.post('/login',login)
+router.get('/logout',logout)
 
 export default router
