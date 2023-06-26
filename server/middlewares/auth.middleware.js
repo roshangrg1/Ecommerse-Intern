60be54a62edb006c93cbde5b41337d34a24a0047
Roshan Guragain <twistfreaky@gmail.com>
2023-06-26 12:19:11 +0545

    Added customRole with correction in token of IsloggedIn

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -3,6 +3,8 @@ import JWT from 'jsonwebtoken'
 import tryCatchHandler from '../services/tryCatchHandler.js'
 import CustomError from '../utils/customError.js'
 
+import AuthRoles from '../utils/authRoles.js'
+
 import config from "../config/index.js"
 
 export const isLoggedIn = tryCatchHandler(async(req,res,next)=>{
@@ -23,9 +25,22 @@ export const isLoggedIn = tryCatchHandler(async(req,res,next)=>{
             const decodedJwtPayload = JWT.verify(token, config.JWT_SECRET)
 
             // _id, find user based on id , set this in req.user
-            req.user = await User.findById(decodedJwtPayload._id, "name email role")
+            req.user = await User.findById(decodedJwtPayload._id, "name email roles")
             next()
         } catch (error){
             throw new CustomError('Not authorized to access this route', 401)
         }
 })
+
+
+export const customRole = (authrole) => {
+    console.log(authrole)
+    return (req, res, next) => {
+      console.log(req.user.roles)
+      if (authrole !== req.user.roles) {
+        return next(new CustomError(`${req.user.roles}You are not allowed for this resouce`, 403));
+      }
+      next();
+    };
+  };
+
