0724e4c71d48b3d4f9f108d1895f9eb9d87df7af
Roshan Guragain <twistfreaky@gmail.com>
2023-06-27 18:42:14 +0545

    Routes added: product routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,18 +1,25 @@
 import express from "express";
 
-import { createProduct,getAllProduct,getOneProduct,adminGetAllProduct  } from "../controllers/product.controller.js";
+import { createProduct,getAllProduct,getOneProduct,adminGetAllProduct, adminUpdateOneProduct , adminDeleteOneProduct } from "../controllers/product.controller.js";
 
 const router =express.Router();
 
-import { isLoggedIn } from '../middlewares/auth.middleware.js';
+import { customRole, isLoggedIn } from '../middlewares/auth.middleware.js';
 import AuthRoles from "../utils/authRoles.js";
 
 // user routes
-router.get('/products',getAllProduct)
+router.route('/products').get(getAllProduct)
+router.route("/product/:id").get(getOneProduct);
 
 // admin
-router.post('/admin/product/add',isLoggedIn,()=>{AuthRoles.ADMIN},createProduct)
+router.post('/admin/product/add',isLoggedIn,customRole(AuthRoles.ADMIN),createProduct)
 
-router.get('/admin/products', isLoggedIn,()=>{AuthRoles.ADMIN},adminGetAllProduct)
+router.get('/admin/products', isLoggedIn,customRole(AuthRoles.ADMIN),adminGetAllProduct)
+
+router
+  .route("/admin/product/:id")
+  .put(isLoggedIn, customRole(AuthRoles.ADMIN), adminUpdateOneProduct)
+  .delete(isLoggedIn, customRole(AuthRoles.ADMIN), adminDeleteOneProduct);
+
 
 export default router
